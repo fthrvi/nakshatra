fthrvi/nakshatra

commit 8dd3687f08f3003e2fa14f377825e570b404f0d1

Optimize RemoteSequenceManager (#106)
- [x] made RemoteSequenceManager into a background thread that pre-fetches information instead of running just in time
- [x] moved routing-related stuff to petals.client.routing
- [x] extract remote peer routing information to RemoteSequenceInfo
- [x] made sure that the code survives continued use (e.g. one hour)
- [x] updated every spot where update_ is called manually
- [x] modified get_sequence to check that the thread is alive, warn if not
- [x] removed max_retries, switched rpc_info to exponential backoff
- [x] fixed a bg that causes RemoteSeq* to lose user-defined hyperparameters (e.g. timeout) upon subsequencing (sequential[3:5])
- [x] moved client-side points strategy to client.routing
- [x] ensured that RemoteSequenceManager thread created in get_remote_module properly shuts down when the module is destroyed
- [x] resolved minor affected todos
- [x] modified tests to no longer use PYTHONPATH
- [x] worked around protocol error in rpc_info


Co-authored-by: Aleksandr Borzunov <borzunov.alexander@gmail.com>
Co-authored-by: Artem Chumachenko <artek.chumak@gmail.com>

src/petals/client/routing/sequence_info.py
@@ -1,0 +1,102 @@
+import dataclasses
+import time
+from typing import Iterable, List, Optional, Sequence, Tuple, Type, TypeVar
+
+from hivemind import get_logger, use_hivemind_log_handler
+
+from petals.data_structures import ModuleUID, RemoteModuleInfo, RemoteSpanInfo, ServerState
+
+use_hivemind_log_handler("in_root_logger")
+logger = get_logger(__file__)
+
+
+T = TypeVar("T")
+
+
+@dataclasses.dataclass
+class RemoteSequenceInfo:
+    """
+    A dataclass that stores general information about which servers hold any given layer;
+    - updated by RemoteSequenceManager in a background thread
+    - accessed by routing strategies in .on_update
+    :note: this class should *not* be modified by RoutingStrategy.on_update to avoid interference between strategies;
+     Any metadata specific to one routing strategy, it should be stored inside that strategy. Any information that
+     is used by most routing strategies should be moved from said strategies to this class.
+    """
+
+    block_uids: Tuple[ModuleUID, ...]
+    block_infos: Tuple[RemoteModuleInfo, ...]  # note: the contents of RemoteModuleInfo can and will be updated
+    spans_by_priority: List[RemoteSpanInfo]
+    spans_containing_block: Tuple[List[RemoteSpanInfo], ...]
+    last_updated_time: float
+
+    @classmethod
+    def make_empty(cls: Type[T], block_uids: Iterable[ModuleUID]) -> T:
+        block_uids = tuple(block_uids)
+        empty_block_infos = tuple(RemoteModuleInfo(uid, {}) for uid in block_uids)
+        empty_spans = tuple([] for _ in range(len(block_uids)))
+        return cls(block_uids, empty_block_infos, [], empty_spans, last_updated_time=-float("inf"))
+
+    def __getitem__(self, ix: slice):
+        assert isinstance(ix, slice)
+        block_uids, block_infos = self.block_uids[ix], self.block_infos[ix]
+        spans_by_priority, spans_containing_block = self.compute_spans(block_infos)
+        return RemoteSequenceInfo(
+            block_uids, block_infos, spans_by_priority, spans_containing_block, self.last_updated_time
+        )
+
+    def __len__(self):
+        return len(self.block_uids)
+
+    def update_(self, new_block_infos: List[Optional[RemoteModuleInfo]]):
+        assert len(new_block_infos) == len(self.block_uids)
+        for block_index, (uid, info) in enumerate(zip(self.block_uids, new_block_infos)):
+            if info is None:
+                logger.debug(f"Found no block info for block {uid}")
+                continue
+            if not isinstance(info, RemoteModuleInfo):
+                logger.warning(f"Unexpected dht entry type for {uid}: {info}")
+                continue
+            if not info.servers:
+                logger.debug(f"Found no active peers for block {uid}")
+                continue
+            if info.uid != uid:
+                logger.warning(f"The DHT entry for {uid} actually points to {info.uid}")
+                continue
+            self.block_infos[block_index].servers = info.servers
+
+        self.spans_by_priority, self.spans_containing_block = self.compute_spans(self.block_infos)
+        self.last_updated_time = time.perf_counter()
+
+    @staticmethod
+    def compute_spans(block_infos: Sequence[RemoteModuleInfo]):
+        closed_spans = []
+        active_spans = {}
+        for block_index, info in enumerate(block_infos):
+            if info is not None:
+                for peer_id, server in info.servers.items():
+                    if server.state != ServerState.ONLINE:
+                        continue
+                    if peer_id not in active_spans:
+                        active_spans[peer_id] = RemoteSpanInfo(start=block_index, end=block_index + 1, peer_id=peer_id)
+                    else:  # peer_id in active_spans
+                        active_spans[peer_id].end = block_index + 1
+
+            for peer_id in list(active_spans.keys()):
+                if (
+                    info is None
+                    or peer_id not in info.servers
+                    or info.servers[peer_id] != ServerState.ONLINE
+                    or block_index == len(block_infos) - 1
+                ):
+                    closed_spans.append(active_spans.pop(peer_id))
+        assert not active_spans, f"spans: {active_spans}"
+
+        closed_spans.sort(key=lambda span: span.end - span.start, reverse=True)
+
+        spans_containing_block = tuple(list() for _ in range(len(block_infos)))
+        for span in closed_spans:
+            for block_index in range(span.start, span.end):
+                spans_containing_block[block_index].append(span)
+
+        return closed_spans, spans_containing_block

tests/test_sequence_manager.py
@@ -1,0 +1,54 @@
+import threading
+import time
+
+import pytest
+import torch
+from hivemind import DHT, get_logger, use_hivemind_log_handler
+from test_utils import *
+
+from petals.client import RemoteSequenceManager, RemoteSequential
+from petals.client.remote_model import DistributedBloomConfig
+from petals.data_structures import UID_DELIMITER
+
+use_hivemind_log_handler("in_root_logger")
+logger = get_logger(__file__)
+
+
+@pytest.mark.forked
+def test_sequence_manager_shutdown():
+    config = DistributedBloomConfig.from_pretrained(MODEL_NAME, initial_peers=INITIAL_PEERS)
+    dht = DHT(initial_peers=config.initial_peers, client_mode=True, start=True)
+    sequential = RemoteSequential(config, dht)
+    shutdown_evt = threading.Event()
+
+    # test RemoteSequential with lossy compression
+    block_uids = [f"{config.dht_prefix}{UID_DELIMITER}{i}" for i in range(config.n_layer)]
+    sequential = RemoteSequential(
+        config,
+        dht,
+        sequence_manager=TestSequenceManager(dht, block_uids, sequential.p2p, _was_shut_down=shutdown_evt, start=True),
+    )
+
+    assert sequential.sequence_manager.is_alive()
+    assert sequential.sequence_manager._thread.ready.is_set()
+    assert not shutdown_evt.is_set()
+    sequential(torch.randn(1, 2, config.hidden_size))
+
+    sequential.sequence_manager.shutdown()
+    del sequential
+    time.sleep(1)
+
+    assert shutdown_evt.is_set()
+
+
+class TestSequenceManager(RemoteSequenceManager):
+    """A sequence manager that signals if it was shut down"""
+
+    def __init__(self, *args, _was_shut_down: threading.Event, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._was_shut_down = _was_shut_down
+
+    def shutdown(self):
+        super().shutdown()
+        assert not self.is_alive()
+        self._was_shut_down.set()

.github/workflows/run-tests.yaml
@@ -104,7 +104,7 @@ jobs:
 
           kill -0 $SERVER1_PID $SERVER2_PID $SERVER3_PID $SERVER4_PID $SERVER5_PID # ensure all servers survived init
 
-          PYTHONPATH=. pytest tests --durations=0 --durations-min=1.0 -v
+          pytest tests --durations=0 --durations-min=1.0 -v
 
           kill -0 $SERVER1_PID $SERVER2_PID $SERVER3_PID $SERVER4_PID $SERVER5_PID # ensure all servers survived tests
 

src/petals/client/__init__.py
@@ -1,5 +1,5 @@
 from petals.client.inference_session import InferenceSession
 from petals.client.remote_model import DistributedBloomConfig, DistributedBloomForCausalLM, DistributedBloomModel
 from petals.client.remote_sequential import RemoteSequential, RemoteTransformerBlock
-from petals.client.sequence_manager import RemoteSequenceManager
-from petals.client.spending_policy import NoSpendingPolicy, SpendingPolicyBase
+from petals.client.routing.sequence_manager import RemoteSequenceManager
+from petals.client.routing.spending_policy import NoSpendingPolicy, SpendingPolicyBase

src/petals/client/inference_session.py
@@ -20,7 +20,7 @@ from hivemind.moe.client.remote_expert_worker import RemoteExpertWorker
 from hivemind.p2p import StubBase
 from hivemind.proto import runtime_pb2
 
-from petals.client.sequence_manager import RemoteSequenceManager
+from petals.client.routing.sequence_manager import RemoteSequenceManager
 from petals.data_structures import CHAIN_DELIMITER, ModuleUID, RemoteSpanInfo, RPCInfo
 from petals.server.handler import TransformerConnectionHandler
 from petals.utils.misc import DUMMY, is_dummy
@@ -44,14 +44,14 @@ class _ServerInferenceSession:
         *,
         timeout: float,
         max_length: int,
-        points: int = 0,
+        **metadata,
     ):
         self.uid, self.rpc_info = uid, rpc_info
         self.num_blocks = uid.count(CHAIN_DELIMITER) + 1
         self._inputs_queue: asyncio.Queue[runtime_pb2.ExpertRequest] = inputs_queue
         self._outputs_stream: AsyncIterator[runtime_pb2.ExpertResponse] = outputs_aiter
         self.timeout = timeout
-        self._serialized_metadata = MSGPackSerializer.dumps(dict(max_length=max_length, points=points))
+        self._serialized_metadata = MSGPackSerializer.dumps(dict(max_length=max_length, **metadata))
         self.stepped = False
         self.closed = False
 
@@ -162,7 +162,7 @@ class InferenceSession:
     An interface to a multi-step *inference* session for a sequence of remote transformer blocks
     """
 
-    def __init__(self, sequence_manager: RemoteSequenceManager, p2p: P2P, max_length: int, **metadata):
+    def __init__(self, sequence_manager: RemoteSequenceManager, p2p: P2P, max_length: int):
         self._sequence_manager = sequence_manager
         self._p2p = p2p
         self._closed = False
@@ -171,7 +171,6 @@ class InferenceSession:
         self._server_inputs = []  # Used in case of server failures to regenerate attention caches on new servers
         self._position = 0
         self._max_length = max_length
-        self._metadata = metadata
 
     def _enter_server_sessions(self, chosen_spans: List[RemoteSpanInfo]) -> List[_ServerInferenceSession]:
         server_sessions = []
@@ -179,6 +178,7 @@ class InferenceSession:
             for span in chosen_spans:
                 stub = TransformerConnectionHandler.get_stub(self._p2p, span.peer_id)
                 span_uids = CHAIN_DELIMITER.join(self._sequence_manager.block_uids[span.start : span.end])
+                metadata = self._sequence_manager.get_request_metadata("rpc_inference", span_uids, peer_id=span.peer_id)
                 session = RemoteExpertWorker.run_coroutine(
                     _ServerInferenceSession.create(
                         stub,
@@ -186,7 +186,7 @@ class InferenceSession:
                         rpc_info=self._sequence_manager.rpc_info,
                         timeout=self._sequence_manager.request_timeout,
                         max_length=self._max_length,
-                        **self._metadata,
+                        **metadata,
                     )
                 )
                 server_sessions.append(session)
@@ -237,7 +237,7 @@ class InferenceSession:
                 logger.debug(f"Inference: block {block_idx}, attempt {attempt_no}")
                 try:
                     if attempt_no >= 1:
-                        self._sequence_manager.update_()
+                        self._sequence_manager.update(wait=True)
                     if not self._chosen_spans or not self._server_sessions or attempt_no >= 1:
                         # If there is a failed server session, this code closes it
                         self._exit_server_sessions(self._server_sessions[server_idx : server_idx + 1])

src/petals/client/remote_model.py
@@ -36,7 +36,7 @@ class DistributedBloomConfig(BloomConfig):
     chunk_size_for_efficient_fp16_on_cpu: int = 10000  # a chunk size for a LM head for efficient half-precision on CPU
     pre_seq_len: int = 0  # a number of tokens for prompt tuning.
     tuning_mode: Optional[str] = None  # One of the finetune options: [None, 'shallow_ptune', 'deep_ptune', 'adapters']
-    request_timeout: int = 20  # a number of seconds for waiting result from each node
+    request_timeout: int = 30  # a number of seconds for waiting result from each node
 
 
 original_register_parameter = nn.Module.register_parameter

src/petals/client/remote_sequential.py
@@ -9,7 +9,7 @@ from torch import nn
 
 import petals.client
 from petals.client.inference_session import InferenceSession
-from petals.client.sequence_manager import RemoteSequenceManager
+from petals.client.routing.sequence_manager import RemoteSequenceManager
 from petals.client.sequential_autograd import _RemoteSequentialAutogradFunction
 from petals.data_structures import UID_DELIMITER
 from petals.utils.misc import DUMMY
@@ -30,7 +30,7 @@ class RemoteSequential(nn.Module):
         dht_prefix: Optional[str] = None,
         p2p: Optional[P2P] = None,
         sequence_manager: Optional[RemoteSequenceManager] = None,
-        request_timeout: int = 20,
+        **kwargs,
     ):
         super().__init__()
         self.config = config
@@ -39,16 +39,18 @@ class RemoteSequential(nn.Module):
         self.p2p = RemoteExpertWorker.run_coroutine(dht.replicate_p2p()) if p2p is None else p2p
 
         num_blocks = self.config.n_layer if sequence_manager is None else len(sequence_manager)
-        block_uids = [f"{config.dht_prefix}{UID_DELIMITER}{i}" for i in range(num_blocks)]
+        block_uids = tuple(f"{config.dht_prefix}{UID_DELIMITER}{i}" for i in range(num_blocks))
         if sequence_manager is None:
             logger.debug(f"Creating new sequence manager for block uids: {block_uids}")
-            self.sequence_manager = RemoteSequenceManager(dht, block_uids, self.p2p, request_timeout=request_timeout)
+            self.sequence_manager = RemoteSequenceManager(dht, block_uids, self.p2p, start=True, **kwargs)
             self.is_subsequence = False
         else:
             logger.debug(f"Reusing sequence manager with {len(sequence_manager)} modules")
+            if kwargs:
+                logger.warning(f"Parameters {kwargs} are ignored because sequence_manager is explicitly provided")
             self.sequence_manager = sequence_manager
-            assert isinstance(sequence_manager.block_uids, list)
-            self.is_subsequence = self.sequence_manager.block_uids != block_uids
+            assert isinstance(sequence_manager.sequence_info.block_uids, tuple)
+            self.is_subsequence = self.sequence_manager.sequence_info.block_uids != block_uids
 
     def forward(self, inputs: torch.Tensor, prompts: torch.Tensor = DUMMY):
         outputs = _RemoteSequentialAutogradFunction.apply(inputs, prompts, self.sequence_manager)
@@ -81,7 +83,6 @@ class RemoteSequential(nn.Module):
         return len(self.sequence_manager)
 
     def inference_session(self, **kwargs) -> InferenceSession:
-        self.sequence_manager.update_()
         return InferenceSession(self.sequence_manager, self.p2p, **kwargs)
 
     def extra_repr(self) -> str:

src/petals/client/routing/__init__.py
@@ -1,0 +1,1 @@
+"""Client-side functions responsible for choosing the best server, """

commit 02441d6dafc6b2cd2e03ade18e77d64687a95279

Hotfix span selection (#110)
Fix an issue in span selection that was introduced in #106

src/petals/client/routing/sequence_info.py
@@ -86,7 +86,7 @@ class RemoteSequenceInfo:
                 if (
                     info is None
                     or peer_id not in info.servers
-                    or info.servers[peer_id] != ServerState.ONLINE
+                    or info.servers[peer_id].state != ServerState.ONLINE
                     or block_index == len(block_infos) - 1
                 ):
                     closed_spans.append(active_spans.pop(peer_id))

tests/test_sequence_manager.py
@@ -29,6 +29,9 @@ def test_sequence_manager_shutdown():
         sequence_manager=TestSequenceManager(dht, block_uids, sequential.p2p, _was_shut_down=shutdown_evt, start=True),
     )
 
+    sequence = sequential.sequence_manager.make_sequence()
+    assert all(sequence[i].peer_id != sequence[i + 1].peer_id for i in range(len(sequence) - 1))
+
     assert sequential.sequence_manager.is_alive()
     assert sequential.sequence_manager._thread.ready.is_set()
     assert not shutdown_evt.is_set()